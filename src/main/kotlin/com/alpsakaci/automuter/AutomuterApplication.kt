3c840fb27844f903a34b31aeacde0614cedf1f37
Alp Sakaci <alpsakaci@protonmail.com>
2022-03-17 14:18:08 +0300

    init project

---
+++ src/main/kotlin/com/alpsakaci/automuter/AutomuterApplication.kt
@@ -0,0 +1,11 @@
+package com.alpsakaci.automuter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AutomuterApplication
+
+fun main(args: Array<String>) {
+	runApplication<AutomuterApplication>(*args)
+}
---
+++ src/test/kotlin/com/alpsakaci/automuter/AutomuterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alpsakaci.automuter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AutomuterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
