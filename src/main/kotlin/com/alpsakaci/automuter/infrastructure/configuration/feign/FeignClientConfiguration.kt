d56d35ac5cd92893af969335e8a9e48172c1c869
Alp Sakaci <alpsakaci@protonmail.com>
2022-04-10 15:56:48 +0300

    impl feign interceptor

--- src/main/kotlin/com/alpsakaci/automuter/infrastructure/configuration/feign/FeignClientConfiguration.kt
+++ src/main/kotlin/com/alpsakaci/automuter/infrastructure/configuration/feign/FeignClientConfiguration.kt
@@ -1,16 +1,13 @@
 package com.alpsakaci.automuter.infrastructure.configuration.feign
 
-import com.alpsakaci.automuter.infrastructure.configuration.feign.decoder.FeignClientErrorDecoder
 import org.springframework.cloud.openfeign.EnableFeignClients
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@EnableFeignClients(value = ["com.alpsakaci.automuter.infrastructure.httpclient"])
+@EnableFeignClients(
+    value = ["com.alpsakaci.automuter.infrastructure.httpclient"]
+)
 class FeignClientConfiguration {
 
-    @Bean
-    fun feignClientErrorDecoder(): FeignClientErrorDecoder {
-        return FeignClientErrorDecoder()
-    }
 }
+
--- src/main/kotlin/com/alpsakaci/automuter/infrastructure/configuration/feign/interceptor/FeignClientRequestInterceptor.kt
+++ src/main/kotlin/com/alpsakaci/automuter/infrastructure/configuration/feign/interceptor/FeignClientRequestInterceptor.kt
@@ -2,11 +2,32 @@ package com.alpsakaci.automuter.infrastructure.configuration.feign.interceptor
 
 import feign.RequestInterceptor
 import feign.RequestTemplate
+import org.springframework.context.annotation.Lazy
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
+import org.springframework.stereotype.Component
 
-class FeignClientRequestInterceptor : RequestInterceptor {
+@Component
+@Lazy
+class FeignClientRequestInterceptor(
+    var clientService: OAuth2AuthorizedClientService
+) : RequestInterceptor {
 
     override fun apply(requestTemplate: RequestTemplate) {
-        TODO("implement feign client interceptor")
-    }
+//        val requestAttributes = RequestContextHolder.getRequestAttributes() as ServletRequestAttributes
+//        val request = requestAttributes.request
+        var authentication = SecurityContextHolder.getContext().authentication
+        val oauthToken = authentication as OAuth2AuthenticationToken
+        val clientRegistrationId = oauthToken.authorizedClientRegistrationId
+        lateinit var accessToken: String
 
+        if (clientRegistrationId.equals("twitter")) {
+            val client = clientService.loadAuthorizedClient<OAuth2AuthorizedClient>(clientRegistrationId, oauthToken.name)
+            accessToken = client.accessToken.tokenValue
+        }
+
+        requestTemplate.header("Authorization", "Bearer $accessToken")
+    }
 }
